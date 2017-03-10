1c737ced8af2b3d748e72993effd1998d96fb96e
MJ <MJ@tsf-484-wpa-0-206.epfl.ch>
2017-03-10 18:26:35 +0100

    sinusoid (exercice 3)

--- hw2-MarcJollèsFabriceGuibertJonathanScanzi/hw2_trackball/grid/grid_vshader.glsl
+++ hw2-MarcJollèsFabriceGuibertJonathanScanzi/hw2_trackball/grid/grid_vshader.glsl
@@ -7,6 +7,8 @@ out vec2 uv;
 uniform mat4 MVP;
 uniform float time;
 
+#define M_PI 3.1415926535897932384626433832795
+
 void main() {
     uv = (position + vec2(1.0, 1.0)) * 0.5;
 
@@ -14,7 +16,12 @@ void main() {
     // plane.
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
-    float height = 0.0;
+
+    float frequency = 2.0f;
+    float amplitude = 0.1f;
+
+    float height = amplitude * sin(2 * M_PI * frequency * (uv.x + uv.y) + time);
+
     vec3 pos_3d = vec3(position.x, height, -position.y);
 
     gl_Position = MVP * vec4(pos_3d, 1.0);
