ab7a17fcd134dc70fd8553b8b5ec1a66bb6303a8
mj2905 <marc.jolles@epfl.ch>
2017-03-11 14:27:13 +0100

    Adds the archive

--- hw2-MarcJollèsFabriceGuibertJonathanScanzi/hw2_trackball/grid/grid_vshader.glsl
+++ hw2-MarcJollèsFabriceGuibertJonathanScanzi/hw2_trackball/grid/grid_vshader.glsl
@@ -7,7 +7,9 @@ out vec2 uv;
 uniform mat4 MVP;
 uniform float time;
 
-#define M_PI 3.1415926535897932384626433832795
+const float M_PI = 3.1415926535897932384626433832795;
+
+#define Water (0)
 
 void main() {
     uv = (position + vec2(1.0, 1.0)) * 0.5;
@@ -17,10 +19,24 @@ void main() {
     // TODO 6: animate the height of the grid points as a sine function of the
     // 'time' and the position ('uv') within the grid.
 
+#if(!Water)
     float frequency = 2.0f;
-    float amplitude = 0.1f;
+    float amplitude = 0.08f;
 
     float height = amplitude * sin(2 * M_PI * frequency * (uv.x + uv.y) + time);
+
+#else
+    float x = uv.x - 0.5;
+    float y = uv.y - 0.5;
+    float r = sqrt(x*x + y*y);
+
+    float attenuation = 1/(1+mod(time/2.0f, 20));
+
+    float frequency = 3.0f;
+    float amplitude = attenuation*0.2f*exp(-3*r);
+
+    float height =  amplitude*sin(2 * M_PI * frequency * r + 10*time);
+#endif
 
     vec3 pos_3d = vec3(position.x, height, -position.y);
 
