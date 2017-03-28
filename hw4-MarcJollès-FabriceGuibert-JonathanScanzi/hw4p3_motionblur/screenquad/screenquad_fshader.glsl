58f66319978a59a7d605bff303d1877c5135f7e6
Jonathan Scanzi <kratosjon@gmail.com>
2017-03-28 18:44:43 +0200

    very small improvmeent to blur shader

--- hw4-MarcJollès-FabriceGuibert-JonathanScanzi/hw4p3_motionblur/screenquad/screenquad_fshader.glsl
+++ hw4-MarcJollès-FabriceGuibert-JonathanScanzi/hw4p3_motionblur/screenquad/screenquad_fshader.glsl
@@ -4,7 +4,7 @@ uniform sampler2D colorTex;
 uniform sampler2D velocityTex;
 out vec4 color;
 
-int nb_samples = 30;
+const int nb_samples = 30;
 
 void main() {
     /// TODO: use the velocity vector stored in velocityTex to compute the line integral
@@ -12,17 +12,17 @@ void main() {
     /// HINT: you can scale the velocity vector to make the motion blur effect more prominent
     /// HINT: to debug integration don't use the velocityTex, simply assume velocity is constant, e.g. vec2(1,0)
 
-    vec2 velocity = 100*texture(velocityTex, uv).xy;
+    vec2 velocity = 50*texture(velocityTex, uv).xy;
 
     ivec2 window_dim = textureSize(colorTex, 0);
 
-    vec2 xy = uv;
+    vec2 xy = uv + (nb_samples/2)*((velocity) / window_dim);
     vec3 c = vec3(0);
     for(int i = 0; i < nb_samples; ++i) {
         c += texture(colorTex, xy).rgb;
         xy -= velocity/window_dim;
     }
-    c /= nb_samples;
+    c /= float(nb_samples);
 
     color = vec4(c, 1);
 }
