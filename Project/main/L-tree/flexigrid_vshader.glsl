4edbc09327df31f12bb054cc0e82a34668a384ec
mj2905 <marc.jolles@epfl.ch>
2017-04-13 11:37:37 +0200

    MVP

--- Project/main/L-tree/flexigrid_vshader.glsl
+++ Project/main/L-tree/flexigrid_vshader.glsl
@@ -12,12 +12,12 @@ out vec2 uv;
 //uniform mat4 view;
 //uniform vec3 light_pos;
 
-//uniform mat4 MVP;
+uniform mat4 MVP;
 //uniform float time;
 
 void main() {
     uv = (position.xy + vec2(1.0, 1.0)) * 0.5;
-    gl_Position = vec4(position, 1.0);
+    gl_Position = MVP * vec4(position, 1.0);
     // convert the 2D position into 3D positions that all lay in a horizontal
     // plane.
 }
